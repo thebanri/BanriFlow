e6a0a211d7e398ad297f05e7e92a410c1d430d6e
thebanri <thebanri@outlook.com>
2026-06-23 09:48:37 +0300

    revert(ui): remove all 3D and test page features

--- remote-ui/src/main.jsx
+++ remote-ui/src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import TestPage from './TestPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
